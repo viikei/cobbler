b5f13e9f75e4604ba464698b6e4654e8205ee933
viikei <62841836+viikei@users.noreply.github.com>
2022-03-18 07:39:39 +0000

    add UinonTech OS vendor
    add UinonTech OS vendor

--- distro_build_configs.sh
+++ distro_build_configs.sh
@@ -32,7 +32,7 @@ if [ "$DISTRO" = "" ] && [ -r /etc/os-release ];then
 	sle*|*suse*)
 	    DISTRO="SUSE"
 	    ;;
-	fedora*|ol*|centos*|rhel*|rocky*)
+	fedora*|ol*|centos*|rhel*|rocky*|uos*)
 	    DISTRO="FEDORA"
 	    ;;
 	ubuntu*|debian*)
